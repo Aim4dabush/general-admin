56b8ffcc85a0aa7636b377b6f1d95f0e28c9ab10
aim4dabush <edarasato@gmail.com>
2023-10-16 15:56:58 -0700

    App - added Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 //3rd party packages
 import { Outlet } from 'react-router-dom';
 
+//components
+import Navbar from './components/navbar/Navbar';
+
 //styles
 import './App.scss';
 
 const App = () => {
   return (
     <>
-      <nav></nav>
+      <nav>
+        <Navbar />
+      </nav>
       <main>
         <Outlet />
       </main>
