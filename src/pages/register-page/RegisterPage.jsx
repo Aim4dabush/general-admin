0bf23d3768338061011600c48b3c931108061f76
aim4dabush <edarasato@gmail.com>
2023-10-16 21:44:57 -0700

    RegisterPage - added RegisterForm component and styled RegisterPage

--- src/pages/register-page/RegisterPage.jsx
+++ src/pages/register-page/RegisterPage.jsx
@@ -1,8 +1,15 @@
+//components
+import RegisterForm from '../../components/register-form/RegisterForm';
+
 //styles
 import styles from './RegisterPage.module.scss';
 
 const RegisterPage = () => {
-  return <section>RegisterPage</section>;
+  return (
+    <section className={styles.page_container}>
+      <RegisterForm />
+    </section>
+  );
 };
 
 export default RegisterPage;
