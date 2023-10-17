aa34751576e8f328fb0e9d03a46c8c60d19ac616
aim4dabush <edarasato@gmail.com>
2023-10-16 19:19:46 -0700

    modal - created NotificationLayout component and scss module

---
+++ src/modal/notification-layout/NotificationLayout.jsx
@@ -0,0 +1,8 @@
+//styles
+import styles from './NotificationLayout.module.scss';
+
+const NotificationLayout = () => {
+  return <div>NotificationLayout</div>;
+};
+
+export default NotificationLayout;
