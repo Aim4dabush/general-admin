455188b711f633d58b2060ae3bcf99b5ff804e52
aim4dabush <edarasato@gmail.com>
2023-10-16 18:44:39 -0700

    utils - created companies and states file

---
+++ src/utils/companies.js
@@ -0,0 +1,22 @@
+export const companyOptions = [
+  {
+    value: '',
+    label: 'Select...',
+  },
+  {
+    value: 'mastercard',
+    label: 'Mastercard',
+  },
+  {
+    value: 'visa',
+    label: 'Visa',
+  },
+  {
+    value: 'discover',
+    label: 'Discover',
+  },
+  {
+    value: 'american_express',
+    label: 'American Express',
+  },
+];
