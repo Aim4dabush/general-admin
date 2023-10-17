eaaae8e89b36d4de4fad14d2a1a2cbd3a0583fd5
aim4dabush <edarasato@gmail.com>
2023-10-16 17:52:45 -0700

    SharedButton - styled component

--- src/components/shared/shared-button/SharedButton.jsx
+++ src/components/shared/shared-button/SharedButton.jsx
@@ -1,8 +1,18 @@
 //styles
 import styles from './SharedButton.module.scss';
 
-const SharedButton = () => {
-  return <button></button>;
+const SharedButton = ({ children, clickHandler, fullWidth, navButton }) => {
+  const fullWidthStyle = fullWidth && styles.full_width;
+  const navButtonStyle = navButton && styles.nav_button;
+
+  return (
+    <button
+      className={`${fullWidthStyle} ${navButtonStyle}`}
+      onClick={clickHandler}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default SharedButton;
