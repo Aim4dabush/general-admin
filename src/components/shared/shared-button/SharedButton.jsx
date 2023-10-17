d0d0c7f4f0417484b9454972ea381c637fc3ce7a
aim4dabush <edarasato@gmail.com>
2023-10-16 20:17:51 -0700

    SharedButton - added more styles

--- src/components/shared/shared-button/SharedButton.jsx
+++ src/components/shared/shared-button/SharedButton.jsx
@@ -1,13 +1,24 @@
 //styles
 import styles from './SharedButton.module.scss';
 
-const SharedButton = ({ children, clickHandler, fullWidth, navButton }) => {
+const SharedButton = ({
+  children,
+  clickHandler,
+  danger,
+  fullWidth,
+  navButton,
+  success,
+  warning,
+}) => {
+  const dangerButtonStyle = danger && styles.danger_button;
   const fullWidthStyle = fullWidth && styles.full_width;
   const navButtonStyle = navButton && styles.nav_button;
+  const successButtonStyle = success && styles.success_button;
+  const warningButtonStyle = warning && styles.warning_button;
 
   return (
     <button
-      className={`${fullWidthStyle} ${navButtonStyle}`}
+      className={`${dangerButtonStyle} ${fullWidthStyle} ${navButtonStyle} ${successButtonStyle} ${warningButtonStyle}`}
       onClick={clickHandler}
     >
       {children}
