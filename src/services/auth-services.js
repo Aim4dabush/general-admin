e5caf0ebfebfb6a4e166c2e5a132891d9e798ad7
aim4dabush <edarasato@gmail.com>
2023-10-16 22:31:12 -0700

    auth-service - added loginUser and signOutUser method

--- src/services/auth-services.js
+++ src/services/auth-services.js
@@ -9,6 +9,45 @@ import {
 } from '../redux/slices/notificationSlice';
 import { setAuth, setAuthReset, setProfile } from '../redux/slices/userSlice';
 
+//POST login data for authentication
+export const loginUser = (data) => {
+  return async (dispatch) => {
+    try {
+      const res = await axios.post('http://localhost:3000/login', data);
+
+      if (res.statusText !== 'Created') {
+        return;
+      }
+
+      const resUser = res.data.user;
+      const resToken = res.data.token;
+      const resMessage = res.data.message;
+      const expiration = DateTime.now().plus({ hours: 2 }).toMillis();
+      const credentials = {
+        id: resUser._id,
+        token: resToken,
+        expiration,
+      };
+      const user = {
+        address: resUser.address,
+        birthday: resUser.birthday,
+        email: resUser.email,
+        fistName: resUser.fistName,
+        lastName: resUser.lastName,
+      };
+
+      localStorage.setItem('credentials', JSON.stringify(credentials));
+      dispatch(setAuth(credentials));
+      dispatch(setProfile(user));
+      dispatch(setNotification({ message: resMessage, status: 'success' }));
+      dispatch(setShowNotification());
+    } catch (err) {
+      dispatch(setNotification({ message: err.message, status: 'error' }));
+      dispatch(setShowNotification());
+    }
+  };
+};
+
 //POST register data to MongoDB
 export const registerUser = (data) => {
   return async (dispatch) => {
@@ -28,5 +67,20 @@ export const registerUser = (data) => {
       dispatch(setNotification({ message: err.message, status: 'error' }));
       dispatch(setShowNotification());
     }
+  };
+};
+
+//clear local storage and auth state
+export const signOutUser = () => {
+  return (dispatch) => {
+    localStorage.clear();
+    dispatch(setAuthReset());
+    dispatch(
+      setNotification({
+        message: 'User Sign Out Successful',
+        status: 'success',
+      })
+    );
+    dispatch(setShowNotification());
   };
 };
