fb85afa10d094ba742c27437f7c4672fe25986fa
aim4dabush <edarasato@gmail.com>
2023-10-16 20:18:37 -0700

    LoginForm - styled component

--- src/components/login-form/LoginForm.jsx
+++ src/components/login-form/LoginForm.jsx
@@ -1,8 +1,23 @@
+//components
+import SharedButton from '../shared/shared-button/SharedButton';
+import SharedInput from '../shared/shared-input/SharedInput';
+
 //styles
 import styles from './LoginForm.module.scss';
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return (
+    <form className={styles.login_form}>
+      <h3 className={styles.login_title}>User Login</h3>
+      <SharedInput>Email</SharedInput>
+      <SharedInput>Password</SharedInput>
+      <div className={styles.button_wrapper}>
+        <SharedButton success={true} fullWidth={true}>
+          Login
+        </SharedButton>
+      </div>
+    </form>
+  );
 };
 
 export default LoginForm;
