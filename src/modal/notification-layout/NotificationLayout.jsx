fa82bcd9ecaec23906f3f6742be077e892b5f34e
aim4dabush <edarasato@gmail.com>
2023-10-16 19:24:08 -0700

    NotificationLayout - styled component

--- src/modal/notification-layout/NotificationLayout.jsx
+++ src/modal/notification-layout/NotificationLayout.jsx
@@ -1,8 +1,21 @@
+//3rd party packages
+import { useSelector } from 'react-redux';
+
 //styles
 import styles from './NotificationLayout.module.scss';
 
 const NotificationLayout = () => {
-  return <div>NotificationLayout</div>;
+  const { notification } = useSelector((state) => state.notification);
+  const errorStyle = notification.status === 'error' && styles.error;
+  const successStyle = notification.status === 'success' && styles.success;
+  return (
+    <div
+      className={`${errorStyle} ${styles.notification_container} ${successStyle}`}
+    >
+      <h3 className={styles.status}></h3>
+      <p></p>
+    </div>
+  );
 };
 
 export default NotificationLayout;
