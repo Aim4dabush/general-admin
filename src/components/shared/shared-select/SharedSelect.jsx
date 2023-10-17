0d5c95dd4b4945b52786b88aa6605ded4cd09ba6
aim4dabush <edarasato@gmail.com>
2023-10-16 21:34:14 -0700

    SharedSelect - styled component

--- src/components/shared/shared-select/SharedSelect.jsx
+++ src/components/shared/shared-select/SharedSelect.jsx
@@ -1,8 +1,43 @@
 //styles
 import styles from './SharedSelect.module.scss';
 
-const SharedSelect = () => {
-  return <div>SharedSelect</div>;
+const SharedSelect = ({
+  blurHandler,
+  changeHandler,
+  children,
+  id,
+  selectError,
+  selectOptions,
+  selectValue,
+  message,
+}) => {
+  const borderErrorStyle = selectError && styles.border_error;
+  const textErrorStyle = selectError && styles.text_error;
+
+  return (
+    <div className={styles.select_control}>
+      <label className={`${textErrorStyle}`} htmlFor={id}>
+        {children}
+      </label>
+      <select
+        className={`${borderErrorStyle}`}
+        id={id}
+        name={id}
+        value={selectValue}
+        onBlur={blurHandler}
+        onChange={changeHandler}
+      >
+        {selectOptions.map((option) => {
+          return (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          );
+        })}
+      </select>
+      {selectError && <p className={`${textErrorStyle}`}>{message}</p>}
+    </div>
+  );
 };
 
 export default SharedSelect;
