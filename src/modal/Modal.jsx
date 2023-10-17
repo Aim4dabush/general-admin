ce9760e5cb137ba017d4b616750f282326748ea1
aim4dabush <edarasato@gmail.com>
2023-10-16 19:39:14 -0700

    Modal - added NotificationLayout component and functionality

--- src/modal/Modal.jsx
+++ src/modal/Modal.jsx
@@ -1,5 +1,38 @@
+import { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+//3rd party packages
+import { useDispatch, useSelector } from 'react-redux';
+
+//component
+import NotificationLayout from './notification-layout/NotificationLayout';
+
+//reducers
+import { setShowNotification } from '../redux/slices/notificationSlice';
+
 const Modal = () => {
-  return <div>Modal</div>;
+  const dispatch = useDispatch();
+  const { showNotification } = useSelector((state) => state.notification);
+
+  useEffect(() => {
+    let closeNotification;
+    if (showNotification) {
+      closeNotification = setTimeout(() => {
+        dispatch(setShowNotification());
+      }, 3000);
+    }
+
+    return () => clearTimeout(closeNotification);
+  }, [showNotification]);
+
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <NotificationLayout />,
+        document.getElementById('notification')
+      )}
+    </>
+  );
 };
 
 export default Modal;
