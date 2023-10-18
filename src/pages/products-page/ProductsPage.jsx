e57ad0c82cd0e38baae79a2561fd6abd4ee9e4f9
aim4dabush <edarasato@gmail.com>
2023-10-18 16:20:41 -0700

    ProductsPage - added getProducts method

--- src/pages/products-page/ProductsPage.jsx
+++ src/pages/products-page/ProductsPage.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
+
+//3rd party packages
+import { useDispatch } from 'react-redux';
+
+//services
+import { getProducts } from '../../services/products-service';
+
 //styles
 import styles from './ProductsPage.module.scss';
 
 const ProductsPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
   return <section>ProductsPage</section>;
 };
 
