eb3839eaa288513c92147e688965dd967b295a8f
aim4dabush <edarasato@gmail.com>
2023-10-17 23:51:32 -0700

    SharedButton - added logoutButton prop and logout_button class and styled

--- src/components/shared/shared-button/SharedButton.jsx
+++ src/components/shared/shared-button/SharedButton.jsx
@@ -6,19 +6,21 @@ const SharedButton = ({
   clickHandler,
   danger,
   fullWidth,
+  logoutButton,
   navButton,
   success,
   warning,
 }) => {
   const dangerButtonStyle = danger && styles.danger_button;
   const fullWidthStyle = fullWidth && styles.full_width;
+  const logoutButtonStyle = logoutButton && styles.logout_button;
   const navButtonStyle = navButton && styles.nav_button;
   const successButtonStyle = success && styles.success_button;
   const warningButtonStyle = warning && styles.warning_button;
 
   return (
     <button
-      className={`${dangerButtonStyle} ${fullWidthStyle} ${navButtonStyle} ${successButtonStyle} ${warningButtonStyle}`}
+      className={`${dangerButtonStyle} ${fullWidthStyle} ${logoutButtonStyle} ${navButtonStyle} ${successButtonStyle} ${warningButtonStyle}`}
       onClick={clickHandler}
     >
       {children}
