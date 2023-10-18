1fe5d5c1a3ec6cfc65582b8a1e1fbfb9093e1d16
aim4dabush <edarasato@gmail.com>
2023-10-18 15:55:03 -0700

    store.js - added product reducer to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 //reducers
 import NotificationReducer from './slices/notificationSlice';
+import ProductsReducer from './slices/productsSlice';
 import UserReducer from './slices/userSlice';
 
 //holds all the reducers
 const rootReducer = combineReducers({
   notification: NotificationReducer,
+  products: ProductsReducer,
   user: UserReducer,
 });
 
