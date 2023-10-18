20064081a8844bbeb9b4f70ce0b725f14e0c2bce
aim4dabush <edarasato@gmail.com>
2023-10-17 23:52:06 -0700

    Navbar - add conditional rendering for links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 
 //3rd party packages
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 
 //components
 import SharedButton from '../shared/shared-button/SharedButton';
@@ -17,14 +18,25 @@ import {
   FaUserCircle,
 } from 'react-icons/fa';
 
+//services
+import { signOutUser } from '../../services/auth-services';
+
 //styles
 import styles from './Navbar.module.scss';
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { auth } = useSelector((state) => state.user);
   const [openMenu, setOpenMenu] = useState(false);
 
   const menuHandler = () => {
     setOpenMenu((prev) => !prev);
+  };
+
+  const signOutHandler = () => {
+    dispatch(signOutUser());
+    navigate('/');
   };
 
   return (
@@ -37,15 +49,27 @@ const Navbar = () => {
           openMenu ? styles.open : styles.close
         }`}
       >
-        <SharedLink path={'/shopping-cart'}>
-          <FaShoppingCart /> Shopping Cart
-        </SharedLink>
-        <SharedLink path={'/profile'}>
-          <FaUserCircle /> Profile
-        </SharedLink>
-        <SharedLink path={'/login'}>
-          <FaSignInAlt /> Login
-        </SharedLink>
+        {auth.token && (
+          <SharedLink path={'/shopping-cart'}>
+            <FaShoppingCart /> Shopping Cart
+          </SharedLink>
+        )}
+        {auth.token && (
+          <SharedLink path={'/profile'}>
+            <FaUserCircle /> Profile
+          </SharedLink>
+        )}
+        {auth.token ? (
+          <li>
+            <SharedButton logoutButton={true} clickHandler={signOutHandler}>
+              <FaSignOutAlt /> Logout
+            </SharedButton>
+          </li>
+        ) : (
+          <SharedLink path={'/login'}>
+            <FaSignInAlt /> Login
+          </SharedLink>
+        )}
       </ul>
       <div className={styles.button_wrapper}>
         <SharedButton navButton={true} clickHandler={menuHandler}>
