4f7fd861378108901d9c674bb031e92d3771a02d
aim4dabush <edarasato@gmail.com>
2023-10-17 23:39:15 -0700

    App - added autoLogout after 2 hours

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
+import { useEffect } from 'react';
+
 //3rd party packages
-import { Outlet } from 'react-router-dom';
+import { DateTime } from 'luxon';
+import { useDispatch, useSelector } from 'react-redux';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 //components
 import Modal from './modal/Modal';
 import Navbar from './components/navbar/Navbar';
 
+//services
+import { signOutUser } from './services/auth-services';
+
 //styles
 import './App.scss';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { auth } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    let autoLogout;
+    const expirationTime = auth.expiration - DateTime.now().toMillis();
+
+    if (auth.token && auth.expiration) {
+      autoLogout = setTimeout(() => {
+        dispatch(signOutUser());
+        navigate('/');
+      }, expirationTime);
+    }
+
+    return () => clearTimeout(autoLogout);
+  }, [dispatch, navigate, auth]);
+
   return (
     <>
       <Modal />
