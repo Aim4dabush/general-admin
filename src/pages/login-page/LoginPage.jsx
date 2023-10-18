e2265a35653ff5f1b255e3b29bb5478f9d30dd3e
aim4dabush <edarasato@gmail.com>
2023-10-17 23:31:30 -0700

    LoginPage - added navigation if login succeeds

--- src/pages/login-page/LoginPage.jsx
+++ src/pages/login-page/LoginPage.jsx
@@ -1,3 +1,9 @@
+import { useEffect } from 'react';
+
+//3rd party packages
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 //components
 import LoginForm from '../../components/login-form/LoginForm';
 
@@ -5,6 +11,15 @@ import LoginForm from '../../components/login-form/LoginForm';
 import styles from './LoginPage.module.scss';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const { auth } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (auth.token) {
+      navigate('/');
+    }
+  }, [navigate, auth]);
+
   return (
     <section className={styles.page_container}>
       <LoginForm />
