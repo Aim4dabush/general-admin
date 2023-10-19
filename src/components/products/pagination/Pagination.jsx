9413a3a55b5fb8b0df583cf4eb5e3af6586dc1d5
aim4dabush <edarasato@gmail.com>
2023-10-18 21:36:05 -0700

    Pagination - added styles and functionality to component

--- src/components/products/pagination/Pagination.jsx
+++ src/components/products/pagination/Pagination.jsx
@@ -1,8 +1,38 @@
+//3rd party packages
+import { useDispatch, useSelector } from 'react-redux';
+
+//components
+import SharedButton from '../../shared/shared-button/SharedButton';
+
+//services
+import { getProducts } from '../../../services/products-service';
+
 //styles
 import styles from './Pagination.module.scss';
 
 const Pagination = () => {
-  return <div>Pagination</div>;
+  const dispatch = useDispatch();
+  const { pages } = useSelector((state) => state.products);
+
+  const paginationHandler = (num) => {
+    dispatch(getProducts(num));
+  };
+
+  return (
+    <div className={styles.pagination_container}>
+      {pages.map((pageNum) => {
+        return (
+          <SharedButton
+            key={pageNum}
+            clickHandler={() => paginationHandler(pageNum)}
+            navButton={true}
+          >
+            {pageNum}
+          </SharedButton>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Pagination;
