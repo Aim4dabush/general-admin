b0662826b1f5254e3a4c93e0ba5ee89352311691
aim4dabush <edarasato@gmail.com>
2023-10-18 17:02:44 -0700

    ProductsCard - styled component

--- src/components/products/products-card/ProductsCard.jsx
+++ src/components/products/products-card/ProductsCard.jsx
@@ -1,8 +1,46 @@
+//3rd party packages
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 //styles
 import styles from './ProductsCard.module.scss';
 
 const ProductsCard = () => {
-  return <div>ProductsCard</div>;
+  const { products } = useSelector((state) => state.products);
+
+  return (
+    <>
+      {products.length > 0 &&
+        products.map((product) => {
+          return (
+            <Link key={product._id} to={`/products/${product._id}`}>
+              <div className={styles.product_card}>
+                <div className={styles.product_header}>
+                  <h5>{product.title}</h5>
+                </div>
+                <div className={styles.img_wrapper}>
+                  <img
+                    className={styles.product_img}
+                    src={product.thumbnail}
+                    alt={product.title}
+                  />
+                </div>
+                <div className={styles.product_body}>
+                  <p className={styles.product_description}>
+                    {product.description}
+                  </p>
+                  <p>Brand: {product.brand}</p>
+                  <p>Category: {product.category}</p>
+                  <p>Price: ${product.price}</p>
+                  <p>Stock: {product.stock}</p>
+                  <p>Rating: {product.rating}</p>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+    </>
+  );
 };
 
 export default ProductsCard;
